91352eb423ac73a8b04b131a5ce3d8130a5ecaa1
Cliffordex <36063652+Cliffordex@users.noreply.github.com>
2023-03-02 18:09:35 -0800

    Initial commit

---
+++ dllmain.cpp
@@ -0,0 +1,21 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, L"Hello world!", L"Sample Payload", MB_OK | MB_ICONINFORMATION);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return 1;
+}
+
